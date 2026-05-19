b09072809e996bb8388a5921268139fa55f0cca3
mrahmedapp <bodysbody2000@gmail.com>
2026-05-19 03:59:43 +0000

    اه

--- artifacts/api-server/src/index.ts
+++ artifacts/api-server/src/index.ts
@@ -1,8 +1,5 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
 import app from "./app";
 import { logger } from "./lib/logger";
-import { verifyToken } from "./lib/auth";
 
 const rawPort = process.env["PORT"];
 
@@ -18,36 +15,6 @@ if (Number.isNaN(port) || port <= 0) {
   throw new Error(`Invalid PORT value: "${rawPort}"`);
 }
 
-const httpServer = createServer(app);
-
-const io = new Server(httpServer, {
-  cors: { origin: "*", methods: ["GET", "POST"] },
-  path: "/api/ws/socket.io",
-});
-
-io.use((socket, next) => {
-  const token = socket.handshake.auth["token"] as string | undefined;
-  const apiKey = socket.handshake.auth["apiKey"] as string | undefined;
-  if (apiKey || (token && verifyToken(token))) {
-    next();
-  } else {
-    next(new Error("Unauthorized"));
-  }
-});
-
-io.on("connection", (socket) => {
-  logger.info({ id: socket.id }, "Client connected");
-  socket.on("disconnect", () => {
-    logger.info({ id: socket.id }, "Client disconnected");
-  });
-});
-
-app.set("io", io);
-
-httpServer.listen(port, (err?: Error) => {
-  if (err) {
-    logger.error({ err }, "Error listening on port");
-    process.exit(1);
-  }
+app.listen(port, () => {
   logger.info({ port }, "Server listening");
 });
